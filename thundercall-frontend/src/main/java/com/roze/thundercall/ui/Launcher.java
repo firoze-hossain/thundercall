530100c92bbdfb3bf4400024c8b326ba92933f53
firoze-uhl <firoze.hossain@uhlbd.com>
2026-08-01 11:10:59 +0600

    logo for mac for packaging

---
+++ thundercall-frontend/src/main/java/com/roze/thundercall/ui/Launcher.java
@@ -0,0 +1,8 @@
+// src/main/java/com/roze/thundercall/ui/Launcher.java
+package com.roze.thundercall.ui;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
